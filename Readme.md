77f6f9bc43a510bc61ecd895c334179dc866e064
batiuszkamaroz <dawid.prajzner01@gmail.com>
2023-03-21 13:12:33 +0100

    ok

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Setup
+
+1. Odpal Keycloak.
+1. Idź do themes/readme i podążaj za instrukcjami.
+1. Przekopiuj JAR i rozpakuj unzip.
+1. Gotowe.
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./themes/youinq/login/*.{ftl,properties}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
